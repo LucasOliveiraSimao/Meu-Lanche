770da6935e8042a3852c6ffa2514392cfb863e09
Lucas <oliveira.lucas20@gmail.com>
2022-09-14 16:55:36 -0400

    Adds dependencies

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,12 @@
+object Version{
+    const val core_ktx = "1.8.0"
+    const val appcompat = "1.5.1"
+    const val material = "1.6.1"
+    const val constraint_layout = "2.1.4"
+    const val navigation = "2.5.1"
+}
+
+object TestVersion{
+    const val junit = "4.13.2"
+    const val test_ext = "1.1.3"
+}
