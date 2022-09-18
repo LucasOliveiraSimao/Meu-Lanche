d31bc9c7526a179f81f901361e145f9d9700264e
Lucas <oliveira.lucas20@gmail.com>
2022-09-17 20:48:37 -0400

    Feat: Implements costumer's home screen

---
+++ app/src/main/java/com/lucassimao/meulanche/model/EstablishmentModel.kt
@@ -0,0 +1,28 @@
+package com.lucassimao.meulanche.model
+
+import androidx.recyclerview.widget.DiffUtil
+
+data class EstablishmentModel(
+    val id: Int,
+    val name: String,
+    val category: String,
+    val delivery_time: String,
+    val delivery_rate: String,
+    val state: String
+) {
+    companion object : DiffUtil.ItemCallback<EstablishmentModel>() {
+        override fun areItemsTheSame(
+            oldItem: EstablishmentModel,
+            newItem: EstablishmentModel
+        ): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(
+            oldItem: EstablishmentModel,
+            newItem: EstablishmentModel
+        ): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
