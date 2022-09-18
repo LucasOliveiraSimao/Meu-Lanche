35f0e1652cd61e5539186f3e0fb9fb1acd690539
Lucas <oliveira.lucas20@gmail.com>
2022-09-17 22:17:05 -0400

    Feat: Implements establishment's home screen

---
+++ app/src/establishment/java/meulanche/model/ProductModel.kt
@@ -0,0 +1,26 @@
+package meulanche.model
+
+import androidx.recyclerview.widget.DiffUtil
+
+data class ProductModel(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val price: String
+) {
+    companion object : DiffUtil.ItemCallback<ProductModel>() {
+        override fun areItemsTheSame(
+            oldItem: ProductModel,
+            newItem: ProductModel
+        ): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(
+            oldItem: ProductModel,
+            newItem: ProductModel
+        ): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
